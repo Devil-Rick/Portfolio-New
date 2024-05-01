79515d659d9803f6eb2a92c14eb4f45aad68cefe
Devil-Rick <saptarshinaruto@gmail.com>
2024-05-01 23:40:19 +0530

    Projects UI done

--- src/data/project-data.js
+++ src/data/project-data.js
@@ -1,23 +1,26 @@
 import contactImg  from '../assets/images/Contacts.png'
+import habitImg from '../assets/images/habitTracker.png'
+import mealImg from '../assets/images/Meals.png'
+import placementImg from '../assets/images/Placement.png'
 
 export const projects = [
     {
-        name: 'Contacts Page',
+        name: 'Contacts WebApp',
         image: contactImg,
         link: 'https://contacts-app-sage-eta.vercel.app',
         git: 'https://github.com/Devil-Rick/Contacts-App'
     },
     {
         name: 'Habit Tracker',
-        image: contactImg,
-        link: 'https://contacts-app-sage-eta.vercel.app',
-        git: 'https://github.com/Devil-Rick/Contacts-App'
+        image: habitImg,
+        link: 'https://habit-tracker-app-1c9d.onrender.com/',
+        git: 'https://github.com/Devil-Rick/Habit-Tracker-App'
     },
     {
-        name: 'Quiz Api',
-        image: contactImg,
-        link: 'https://contacts-app-sage-eta.vercel.app',
-        git: 'https://github.com/Devil-Rick/Contacts-App'
+        name: 'Placement Cell',
+        image: placementImg,
+        link: 'https://placement-cell-9luu.onrender.com/',
+        git: 'https://github.com/Devil-Rick/Placement-CN'
     },
     {
         name: 'Portfolio Site',
@@ -26,9 +29,9 @@ export const projects = [
         git: 'https://github.com/Devil-Rick/Contacts-App'
     },
     {
-        name: 'Contacts Page',
-        image: contactImg,
-        link: 'https://contacts-app-sage-eta.vercel.app',
-        git: 'https://github.com/Devil-Rick/Contacts-App'
+        name: 'Meals App',
+        image: mealImg,
+        link: 'https://exquisite-biscuit-5fb9f0.netlify.app/',
+        git: 'https://github.com/Devil-Rick/CN-mealApp'
     },
 ];
